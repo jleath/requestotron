09a999060d3cd98461a76b5bc259fe5b304224ee
Wes Anderson <wesley.herb.anderson@gmail.com>
2022-02-02 17:55:35 -0500

    added createBinHandler

--- url-gen/controllers/router.js
+++ url-gen/controllers/router.js
@@ -1,3 +1,28 @@
 const requestRouter = require('express').Router();
+const config = require('../util/config');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: config.DB_HOST,
+    user: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME,
+});
+
+const createBinHandler = async (req, res) => {
+    const args = ["fhfh5"];
+    const query = `
+    INSERT INTO bins (url)
+    VALUES ($1)
+    RETURNING url;`;
+    try {
+        const { rows } = await pool.query(query, args);
+        res.status(201).json(rows[0]);
+    } catch (err) {
+        res.status(500).json(error);
+    }
+};
+
+requestRouter.post('/', createBinHandler);
 
 module.exports = requestRouter;
--- url-gen/util/config.js
+++ url-gen/util/config.js
@@ -3,9 +3,17 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
 const LOG_FILE_PATH = process.env.LOG_FILE_PATH;
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 
 module.exports = {
   PORT,
   LOG_FILE_PATH,
   MONGODB_URI,
+  DB_USER,
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
 };
