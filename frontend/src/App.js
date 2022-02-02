3bca81ebd79062aa37b4782dc665e48fdfdfc2ed
Joshua Leath <lawnfurniture@icloud.com>
2022-02-01 19:08:26 -0500

    set up react template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-
-const URL_REQUEST_URI =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3004' : 'https://requestotron.inspect.joshleath.com';
-
+import React from 'react';
 const App = () => {
-  const [ loggingUrl, setLoggingUrl ] = useState('');
-
-  const getLoggerUrl = async (event) => {
-    event.preventDefault();
-    const response = await axios.get(URL_REQUEST_URI);
-    setLoggingUrl(response.data.loggedUrl);
-  };
-
   return (
     <div>
-      <form onSubmit={getLoggerUrl}>
-        <button type="submit">Get URL</button>
-      </form>
-      <p>{loggingUrl ? `${URL_REQUEST_URI}/${loggingUrl}` : ''}</p>
-      <p>{loggingUrl ? `${URL_REQUEST_URI}/inspect/${loggingUrl}` : ''}</p>
+      <p>Howdy pardner</p>
     </div>
   );
 };
