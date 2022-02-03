ef399da5b56bf54b803d3a4025ecf8681f5dd4c8
Will <wor101@gmail.com>
2022-02-02 19:21:11 -0500

    added getBinHandler to confirm bin exists

--- url-gen/controllers/router.js
+++ url-gen/controllers/router.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     host: config.DB_HOST,
-    user: config.DB_USERNAME,
+    user: config.DB_USER,
     password: config.DB_PASSWORD,
     database: config.DB_NAME,
 });
@@ -21,10 +21,40 @@ const createBinHandler = async (req, res) => {
         const { rows } = await pool.query(query, args);
         res.status(201).json(rows[0]);
     } catch (err) {
-        res.status(500).json(error);
+        console.log('Im an error from createBinHandler!');
+        console.log(err)
+        res.status(500).json(err.message);
     }
 };
 
+const getBinId= async (url) => {
+  const query = `SELECT id FROM bins WHERE url=$1;`;
+  
+  const { rows } = await pool.query(query, [url]);
+
+  return parseInt(rows[0].id, 10);
+}
+
+const getBinHandler = async (req, res, next) => {
+  if (req.query.inspect === undefined) {
+    next();
+  }
+  let binId;
+  const url = req.params.url;
+  try {
+    binId = await getBinId(url);
+    console.log("Bin Id:", binId)
+    res.status(200).json(binId);
+
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(404);
+  }
+
+}
+
 requestRouter.post('/', createBinHandler);
+
+requestRouter.get('/:url',getBinHandler );
 
 module.exports = requestRouter;
